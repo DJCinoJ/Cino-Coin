3458e5b81f34815c24572b2c1aba4c9022a47b2c
DJCinoJ1 <44583582+DJCinoJ@users.noreply.github.com>
2019-01-08 18:08:10 +0100

    Delete 2_deploy_contracts.js

--- token_sale/migrations/2_deploy_contracts.js
+++
@@ -1,9 +0,0 @@
-var CinoCoin = artifacts.require("./CinoCoin.sol");
-var CinoCoinSale = artifacts.require("./CinoCoinSale.sol");
-
-module.exports = function(deployer) {
-  deployer.deploy(CinoCoin, 1000000).then(function() {
-    var tokenPrice = 1000000000000000;
-    return deployer.deploy(CinoCoinSale, CinoCoin.address, tokenPrice);
-  });
-};
